e0f437fe4d73069e5929fcda344cc6192902db14
sqwd12312 <411319775@qq.com>
2020-07-07 17:05:53 +0800

    实现了首页界面滑动时隐藏和显示头部、底部导航栏，但头部导航栏下的tabs一直显示的功能

--- app/src/main/java/com/example/zhihu/MainActivity.java
+++ app/src/main/java/com/example/zhihu/MainActivity.java
@@ -1,30 +1,22 @@
 package com.example.zhihu;
 
-import android.os.Build;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.Toast;
-
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationItem;
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
 import com.luseen.luseenbottomnavigation.BottomNavigation.OnBottomNavigationItemClickListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private Button recommendBtn;
-
-    private Button followBtn;
-
-    private Button hotListBtn;
+public class MainActivity extends AppCompatActivity{
 
     private BottomNavigationView bottomNavigationView;
+
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +24,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         //初始化控件
         initControl();
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
-            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS |
-                    localLayoutParams.flags);
-        }
+        //默认显示“推荐”fragment
         replaceFragment(new RecommendFragment());
     }
 
     //初始化控件
     private void initControl(){
-        recommendBtn = (Button)findViewById(R.id.recommend_btn);
-        followBtn = (Button)findViewById(R.id.follow_btn);
-        hotListBtn = (Button)findViewById(R.id.hot_list_btn);
-        recommendBtn.setOnClickListener(this);
-        followBtn.setOnClickListener(this);
-        hotListBtn.setOnClickListener(this);
         //初始化底部导航栏
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
         BottomNavigationItem bottomNavigationItem = new BottomNavigationItem
@@ -68,29 +50,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //禁用背景变化
         bottomNavigationView.isColoredBackground(false);
 //        bottomNavigationView.setItemActiveColorWithoutColoredBackground(R.color.orange);
-        //导航栏监听
+        //底部导航栏监听
         bottomNavigationView.setOnBottomNavigationItemClickListener(new OnBottomNavigationItemClickListener() {
             @Override
             public void onNavigationItemClick(int index) {
                 Toast.makeText(MainActivity.this, "Item " +index +" clicked", Toast.LENGTH_SHORT).show();
             }
         });
-    }
+        //设置三个tab
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.addTab(mTabLayout.newTab().setText("关注"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("推荐"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("热榜"));
+        //默认选中"推荐"tab
+        mTabLayout.getTabAt(1).select();
+        //设置mTabLayout点击监听
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getText().equals("关注")) {
+                    replaceFragment(new FollowFragment());
+                }else if (tab.getText().equals("推荐")){
+                    replaceFragment(new RecommendFragment());
+                }else if (tab.getText().equals("热榜")){
+                    replaceFragment(new HotListFragment());
+                }
+            }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.recommend_btn:
-                replaceFragment(new RecommendFragment());
-                break;
-            case R.id.follow_btn:
-                replaceFragment(new FollowFragment());
-                break;
-            case R.id.hot_list_btn:
-                replaceFragment(new HotListFragment());
-            default:
-                break;
-        }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
     //替换fragment
